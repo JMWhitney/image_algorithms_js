73a6155dc766c04e5776c34aad72c567dedd42db
JMWhitney <JMWhitney96@gmail.com>
2019-04-19 12:26:31 -0700

    * Added comb sort

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var image = "./flower.jpg";
+var image = "./flower.png";
 
 var img = new Image();
 img.src = image;
@@ -12,11 +12,11 @@ img.onload = function() {
   draw(this);
 }
  
-function distance(x1, y1, z1, x2, y2, z2) {
+function distance(x1, y1, z1, x2=0, y2=0, z2=0) {
   let x = x2 - x1;
   let y = y2 - y1;
   let z = z2 - z1;
-  return Math.sqrt(x*x + y*y + z*z);
+  return Math.round(Math.sqrt(x*x + y*y + z*z));
 }
 
 function draw(img) {
@@ -31,7 +31,7 @@ function draw(img) {
   var drawingData = ctx2.getImageData(0, 0, canvas.width, canvas.height);
   var data2 = drawingData.data;
   
-  var iterate = function() {
+  var replicate_random = function() {
     for (let i = 0; i < data2.length; i += 4) {
       let new_r = Math.floor(Math.random() * 255);
       let new_g = Math.floor(Math.random() * 255);
@@ -58,7 +58,7 @@ function draw(img) {
     setTimeout(iterate, 10);
   };
 
-  var initSort = function() {
+  var bubbleSort = function() {
 
     let n = 1;
 
@@ -66,8 +66,8 @@ function draw(img) {
   
       for (let i = 0; i < data1.length - (n * 4); i += 4) {
   
-        d1 = distance(data1[i], data1[i+1], data1[i+2], 0, 0, 0);
-        d2 = distance(data1[i+4], data1[i+5], data1[i+6], 0, 0, 0);
+        d1 = distance(data1[i], data1[i+1], data1[i+2]);
+        d2 = distance(data1[i+4], data1[i+5], data1[i+6]);
   
         if(d2 < d1) {
           //Create copy so we don't lose the data
@@ -89,12 +89,56 @@ function draw(img) {
       n++;
       ctx2.putImageData(imageData, 0, 0);
     };
-
+    
     setInterval(sort, 0);
+    
+  };
 
-  }
+  var combSort = function() {
+    let gap = data1.length;
+    let shrink = 1.33;
+    let sorted = false;
+
+    while( !sorted ) {
+      debugger;
+      //update the gap for next comb
+      if(gap > 12) gap = (Math.floor( gap/shrink ) + 3) & ~0x03; //Make sure the gap is a multiple of 4;
+      if(gap <= 12 && gap > 4) {gap -= 4}
+      else if( gap <= 4 ) {
+        gap = 4;
+        sorted = true;
+      }
+
+      ctx2.putImageData(imageData, 0, 0);
+      
+      let i = 0
+      while( i + gap < data1.length ) {
+
+        d1 = distance(data1[i], data1[i+1], data1[i+2]);
+        d2 = distance(data1[i+gap], data1[i+gap+1], data1[i+gap+2]);
+  
+        if(d2 < d1) {
+          //Create copy so we don't lose the data
+          let temp = [data1[i], data1[i+1], data1[i+2]];
+  
+          //swap first point with second.
+          data1[i] = data1[i+gap];
+          data1[i+1] = data1[i+gap+1];
+          data1[i+2] = data1[i+gap+2];
+  
+          //swap second point with original first point
+          data1[i+gap] = temp[0];
+          data1[i+gap+1] = temp[1];
+          data1[i+gap+2] = temp[2];
+          
+        }
+        
+        i += 4;
+        
+      }
+    }
+  };
 
   var btn = document.getElementById('func');
-  btn.addEventListener('click', initSort);
-  
+  btn.addEventListener('click', combSort);
 }
